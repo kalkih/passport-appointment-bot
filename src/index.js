5e1820284f88c0100392af156a274a0b02b6f753
Karl Kihlström <its@kvrl.me>
2022-03-19 23:16:47 +0100

    Ditch puppeteer for node-fetch & cheerio

--- src/index.js
+++ src/index.js
@@ -1,53 +1,41 @@
-const puppeteer = require("puppeteer");
 const locations = require("./locations");
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
 const argv = yargs(hideBin(process.argv)).argv;
-const fetch = require("node-fetch");
-const logger = require('./logger')
+const logger = require("./logger");
+const nodeFetch = require("node-fetch");
+const fetchCookie = require("fetch-cookie");
+const cheerio = require("cheerio");
+const fetch = fetchCookie(nodeFetch);
 
-
+const SESSION_COOKIE_NAME = "ASP.NET_VentusBooking_SessionId";
 const BASE_URL = "https://bokapass.nemoq.se/Booking/Booking/Index/blekinge";
 const POST_URL = "https://bokapass.nemoq.se/Booking/Booking/Next/blekinge";
-const HEADLESS_HEIGHT = 800;
-const HEADLESS_WIDTH = 1000;
 
 // TODO: Should make these into an option / environment variable
 const LOCATIONS_TO_CHECK = ["Karlskrona", "Karlshamn"];
 const MAX_DATE = "2022-05-01";
+const NUMBER_OF_PEOPLE = 1;
+
+const requestOptions = {
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+};
 
 const locationQueue = [];
-
-const stepQueue = [];
+let sessionId = undefined;
 
 (async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    args: [`--window-size=${HEADLESS_WIDTH},${HEADLESS_HEIGHT}`],
-    defaultViewport: {
-      width: HEADLESS_WIDTH,
-      height: HEADLESS_HEIGHT,
-    },
-    devtools: true,
-  });
-
-  const page = await browser.newPage();
-
   if (argv.session) {
-    logger.info("Using session cookie:", argv.session);
-    await page.setCookie({
-      name: "ASP.NET_VentusBooking_SessionId",
-      value: argv.session,
-      domain: "bokapass.nemoq.se",
-    });
+    logger.info("Using provided session cookie:", argv.session);
+    sessionId = argv.session;
   } else {
-    stepQueue.push(...introSteps(page));
+    logger.info("Getting session cookie...");
+    await setCookie();
   }
 
-  await page.setDefaultNavigationTimeout(0);
-  logger.info("Starting booking session");
-  await page.goto(BASE_URL);
-
+  logger.info("Validating provided locations...");
   for (const location of LOCATIONS_TO_CHECK) {
     if (locations[location]) {
       locationQueue.push(locations[location]);
@@ -56,109 +44,121 @@ const stepQueue = [];
     }
   }
 
-  for await (const { name, navigation } of stepQueue) {
-    await handleStep(page, name, navigation);
-  }
+  await initSession();
 
   logger.info("Starting to check for available timeslots");
-  checkAvailableSlotsForLocation(page, locationQueue[0]);
+  checkAvailableSlotsForLocation(locationQueue[0]);
 })();
 
-const handleStep = async (page, name, callback) => {
-  logger.info(`Started step: ${name}`);
-  await page.waitForSelector("h1");
-  const textContent = await page.evaluate(
-    () => document.querySelector("h1").textContent
-  );
-
-  if (textContent === "504 Gateway Time-out") {
-    logger.warn("504 Gateway Time-out: Retrying...");
-    await page.reload();
-    await handleStep(page, name, callback);
-  }
-  await callback();
-};
-
-const checkAvailableSlotsForLocation = async (page, location) => {
+const checkAvailableSlotsForLocation = async (location) => {
   logger.info(`Switching to location: ${location}`);
   const maxDate = new Date(MAX_DATE);
   const currentDate = new Date();
 
   while (currentDate.getTime() < maxDate.getTime()) {
-    await checkAvailableSlots(page, currentDate);
-    logger.info("Loading next week...");
+    await checkAvailableSlots(currentDate);
     addDays(currentDate);
   }
   logger.verbose("Max date reached, checking next location...");
 
   locationQueue.push(locationQueue.shift());
-  checkAvailableSlotsForLocation(page, locationQueue[0]);
+  checkAvailableSlotsForLocation(locationQueue[0]);
 };
 
-const checkAvailableSlots = async (page, date) => {
-  const cookies = await page.cookies();
-  const cookie = cookies.find(
-    (c) => c.name === "ASP.NET_VentusBooking_SessionId"
-  ).value;
+const checkAvailableSlots = async (date) => {
   logger.info(`Loading week of: ${getShortDate(date)}`);
-  // logger.info(cookie);
-  await fetch(POST_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      Cookie: `ASP.NET_VentusBooking_SessionId=${cookie};`,
-    },
-    redirect: "manual",
-    follow: 0,
-    body: new URLSearchParams({
+  try {
+    const res = await postRequest({
       FormId: 1,
-      NumberOfPeople: 1,
+      NumberOfPeople: NUMBER_OF_PEOPLE,
       RegionId: 0,
       SectionId: locationQueue[0],
       NQServiceTypeId: 1,
       FromDateString: getShortDate(date),
       SearchTimeHour: 12,
-    }),
-  });
-  await page.goto(BASE_URL);
+    });
+    const $ = cheerio.load(await res.text());
 
-  logger.verbose(`Checking week of: ${getShortDate(date)}`);
-  const freeSlots = await page.$$(".timecell script");
-  const bookedSlots = await page.$$(".timecell label");
-  if (freeSlots.length && freeSlots.length > 0) {
-    logger.log("success", `Free timeslots found: ${freeSlots.length}`);
-  } else {
-    logger.verbose(`No free timeslots found, ${bookedSlots.length} reserved`);
+    logger.verbose(`Checking week of: ${getShortDate(date)}`);
+    const freeSlots = $(".timecell script");
+    const bookedSlots = $(".timecell label");
+    if (freeSlots.length && freeSlots.length > 0) {
+      logger.log("success", `Free timeslots found: ${freeSlots.length}`);
+    } else {
+      logger.verbose(`No free timeslots found, ${bookedSlots.length} reserved`);
+    }
+  } catch {
+    logger.verbose(`Failed checking week of: ${getShortDate(date)}`);
   }
 };
 
-const introSteps = (page) => [
-  {
-    name: "New booking session",
-    navigation: async () => {
-      await page.click('[name="StartNextButton"]');
-      await page.waitForNavigation();
-    },
-  },
-  {
-    name: "Cookie consent",
-    navigation: async () => {
-      await page.click('[name="AcceptInformationStorage"]');
-      await page.click('[name="Next"]');
-      await page.waitForNavigation();
-    },
-  },
-  {
-    name: "Residency information",
-    navigation: async () => {
-      await page.click(
-        '[name="ServiceCategoryCustomers[0].ServiceCategoryId"]'
-      );
-      await page.click('[name="Next"]');
-      await page.waitForNavigation();
-    },
-  },
-];
+async function postRequest(body) {
+  try {
+    const response = await fetch(POST_URL, {
+      method: "POST",
+      ...requestOptions,
+      body: new URLSearchParams(body),
+    });
+
+    if (response.ok) {
+      return response;
+    }
+
+    throw new Error("Something went wrong.");
+  } catch {
+    logger.error(response.status);
+    handleFormPost(body);
+  }
+}
+
+async function setCookie() {
+  try {
+    const response = await fetch(BASE_URL);
+
+    if (response.ok) {
+      const cookies = `; ${response.headers.get("set-cookie")}`;
+      const cookieParts = cookies.split(`; ${SESSION_COOKIE_NAME}=`);
+      if (cookieParts.length === 2) {
+        sessionId = cookieParts.pop().split(";").shift();
+        logger.log("success", "Cookie set");
+        return sessionId;
+      }
+    }
+
+    throw new Error(response.status);
+  } catch {
+    logger.error("Failed setting cookie, trying again...");
+    await setCookie();
+  }
+}
+
+async function initSession() {
+  logger.info("Starting booking session...");
+  await postRequest({
+    FormId: 1,
+    ServiceGroupId: 74,
+    StartNextButton: "Boka ny tid",
+  });
+  logger.log("success", "Started booking session");
+
+  logger.info("Accepting booking terms...");
+  await postRequest({
+    AgreementText: "123",
+    AcceptInformationStorage: true,
+    // AcceptInformationStorage: false,
+    NumberOfPeople: NUMBER_OF_PEOPLE,
+    Next: "Nästa",
+  });
+  logger.log("success", "Accepted booking terms");
+
+  logger.info("Setting residency...");
+  await postRequest({
+    "ServiceCategoryCustomers[0].CustomerIndex": 0,
+    "ServiceCategoryCustomers[0].ServiceCategoryId": 2,
+    Next: "Nästa",
+  });
+  logger.log("success", "Residency set");
+}
 
 const getShortDate = (date) => {
   return date.toISOString().split("T")[0];
