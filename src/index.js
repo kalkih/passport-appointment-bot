cb455cdfc83d3f4a515ab76c0f804ead14f62363
Karl Kihlström <karl.kihlstrom@lysa.se>
2022-03-19 21:29:56 +0100

    Week switching & available timeslot checks

--- src/index.js
+++ src/index.js
@@ -3,8 +3,46 @@ const locations = require("./locations");
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
 const argv = yargs(hideBin(process.argv)).argv;
+const fetch = require("node-fetch");
+const { transports, createLogger, format } = require("winston");
+
+const levels = {
+  error: 0,
+  warn: 1,
+  success: 2,
+  info: 3,
+  verbose: 4,
+  debug: 5,
+};
+
+const colors = {
+  error: "red",
+  warn: "yellow",
+  success: "white bold",
+  info: "green",
+  verbose: "blue",
+  debug: "magenta",
+};
+
+const logger = createLogger({
+  level: "debug",
+  levels,
+  transports: [
+    new transports.Console({
+      colorize: true,
+      format: format.combine(
+        format.colorize({ all: true, colors }),
+        format.timestamp({ format: "HH:mm:ss" }),
+        format.printf(
+          (info) => `[${info.timestamp}] [${info.level}]\t${info.message}`
+        )
+      ),
+    }),
+  ],
+});
 
 const BASE_URL = "https://bokapass.nemoq.se/Booking/Booking/Index/blekinge";
+const POST_URL = "https://bokapass.nemoq.se/Booking/Booking/Next/blekinge";
 const HEADLESS_HEIGHT = 800;
 const HEADLESS_WIDTH = 1000;
 
@@ -24,13 +62,13 @@ const stepQueue = [];
       width: HEADLESS_WIDTH,
       height: HEADLESS_HEIGHT,
     },
-    // devtools: true,
+    devtools: true,
   });
 
   const page = await browser.newPage();
 
   if (argv.session) {
-    console.info("Using session cookie:", argv.session);
+    logger.info("Using session cookie:", argv.session);
     await page.setCookie({
       name: "ASP.NET_VentusBooking_SessionId",
       value: argv.session,
@@ -41,14 +79,14 @@ const stepQueue = [];
   }
 
   await page.setDefaultNavigationTimeout(0);
-  console.info("Starting booking session");
+  logger.info("Starting booking session");
   await page.goto(BASE_URL);
 
   for (const location of LOCATIONS_TO_CHECK) {
     if (locations[location]) {
       locationQueue.push(locations[location]);
     } else {
-      console.warn("Location not found:", location);
+      logger.error(`Location not found: ${location}, skipping...`);
     }
   }
 
@@ -56,19 +94,19 @@ const stepQueue = [];
     await handleStep(page, name, navigation);
   }
 
-  console.info("Time to start checking for available timeslots");
+  logger.info("Starting to check for available timeslots");
   checkAvailableSlotsForLocation(page, locationQueue[0]);
 })();
 
 const handleStep = async (page, name, callback) => {
-  console.log("Started step:", name);
+  logger.info(`Started step: ${name}`);
   await page.waitForSelector("h1");
   const textContent = await page.evaluate(
     () => document.querySelector("h1").textContent
   );
 
   if (textContent === "504 Gateway Time-out") {
-    console.warn("504 Gateway Time-out: Retrying...");
+    logger.warn("504 Gateway Time-out: Retrying...");
     await page.reload();
     await handleStep(page, name, callback);
   }
@@ -76,30 +114,56 @@ const handleStep = async (page, name, callback) => {
 };
 
 const checkAvailableSlotsForLocation = async (page, location) => {
-  console.info("Switching to location:", location);
+  logger.info(`Switching to location: ${location}`);
   const maxDate = new Date(MAX_DATE);
   const currentDate = new Date();
-  await page.select('[name="SectionId"]', location.toString());
 
   while (currentDate.getTime() < maxDate.getTime()) {
     await checkAvailableSlots(page, currentDate);
-    console.info("Loading next week...");
+    logger.info("Loading next week...");
     addDays(currentDate);
   }
-  console.info("Max date reached, checking next location");
+  logger.verbose("Max date reached, checking next location...");
 
   locationQueue.push(locationQueue.shift());
-  checkAvailableSlotsForLocation(locationQueue[0]);
+  checkAvailableSlotsForLocation(page, locationQueue[0]);
 };
 
 const checkAvailableSlots = async (page, date) => {
-  await page.$eval('[name="FromDateString"]', (el) => (el.value = ""));
-  await page.type('[name="FromDateString"]', getShortDate(date));
-  await page.click('[name="TimeSearchButton"]');
-  await page.waitForNavigation();
+  const cookies = await page.cookies();
+  const cookie = cookies.find(
+    (c) => c.name === "ASP.NET_VentusBooking_SessionId"
+  ).value;
+  logger.info(`Loading week of: ${getShortDate(date)}`);
+  // logger.info(cookie);
+  await fetch(POST_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Cookie: `ASP.NET_VentusBooking_SessionId=${cookie};`,
+    },
+    redirect: "manual",
+    follow: 0,
+    body: new URLSearchParams({
+      FormId: 1,
+      NumberOfPeople: 1,
+      RegionId: 0,
+      SectionId: locationQueue[0],
+      NQServiceTypeId: 1,
+      FromDateString: getShortDate(date),
+      SearchTimeHour: 12,
+    }),
+  });
+  await page.goto(BASE_URL);
 
-  console.info("Checking week of:", getShortDate(date));
-  // TODO: Check for available timeslots here
+  logger.verbose(`Checking week of: ${getShortDate(date)}`);
+  const freeSlots = await page.$$(".timecell script");
+  const bookedSlots = await page.$$(".timecell label");
+  if (freeSlots.length && freeSlots.length > 0) {
+    logger.log("success", `Free timeslots found: ${freeSlots.length}`);
+  } else {
+    logger.verbose(`No free timeslots found, ${bookedSlots.length} reserved`);
+  }
 };
 
 const introSteps = (page) => [
