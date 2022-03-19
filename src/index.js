5d368b86a4f799458d122a19b5c9d632ad0685ea
Karl Kihlström <its@kvrl.me>
2022-03-19 21:36:01 +0100

    Break out logger

--- src/index.js
+++ src/index.js
@@ -4,42 +4,8 @@ const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
 const argv = yargs(hideBin(process.argv)).argv;
 const fetch = require("node-fetch");
-const { transports, createLogger, format } = require("winston");
+const logger = require('./logger')
 
-const levels = {
-  error: 0,
-  warn: 1,
-  success: 2,
-  info: 3,
-  verbose: 4,
-  debug: 5,
-};
-
-const colors = {
-  error: "red",
-  warn: "yellow",
-  success: "white bold",
-  info: "green",
-  verbose: "blue",
-  debug: "magenta",
-};
-
-const logger = createLogger({
-  level: "debug",
-  levels,
-  transports: [
-    new transports.Console({
-      colorize: true,
-      format: format.combine(
-        format.colorize({ all: true, colors }),
-        format.timestamp({ format: "HH:mm:ss" }),
-        format.printf(
-          (info) => `[${info.timestamp}] [${info.level}]\t${info.message}`
-        )
-      ),
-    }),
-  ],
-});
 
 const BASE_URL = "https://bokapass.nemoq.se/Booking/Booking/Index/blekinge";
 const POST_URL = "https://bokapass.nemoq.se/Booking/Booking/Next/blekinge";
---
+++ src/logger.js
@@ -0,0 +1,38 @@
+const { transports, createLogger, format } = require("winston");
+
+const levels = {
+  error: 0,
+  warn: 1,
+  success: 2,
+  info: 3,
+  verbose: 4,
+  debug: 5,
+};
+
+const colors = {
+  error: "red",
+  warn: "yellow",
+  success: "white bold",
+  info: "green",
+  verbose: "blue",
+  debug: "magenta",
+};
+
+const logger = createLogger({
+  level: "debug",
+  levels,
+  transports: [
+    new transports.Console({
+      colorize: true,
+      format: format.combine(
+        format.colorize({ all: true, colors }),
+        format.timestamp({ format: "HH:mm:ss" }),
+        format.printf(
+          (info) => `[${info.timestamp}] [${info.level}]\t${info.message}`
+        )
+      ),
+    }),
+  ],
+});
+
+module.exports = logger;
