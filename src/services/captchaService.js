9f597621ca18f64fc8f456750cd32e274cc4333e
Karl Kihlström <karl.kihlstrom@lysa.se>
2022-04-03 12:54:01 +0200

    manually require files to fix issues with pkg

--- src/services/captchaService.js
+++ src/services/captchaService.js
@@ -1,4 +1,23 @@
 const puppeteer = require("puppeteer-extra");
+
+require("puppeteer-extra-plugin-stealth/evasions/chrome.app");
+require("puppeteer-extra-plugin-stealth/evasions/chrome.csi");
+require("puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes");
+require("puppeteer-extra-plugin-stealth/evasions/chrome.runtime");
+require("puppeteer-extra-plugin-stealth/evasions/defaultArgs");
+require("puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow");
+require("puppeteer-extra-plugin-stealth/evasions/media.codecs");
+require("puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency");
+require("puppeteer-extra-plugin-stealth/evasions/navigator.languages");
+require("puppeteer-extra-plugin-stealth/evasions/navigator.permissions");
+require("puppeteer-extra-plugin-stealth/evasions/navigator.plugins");
+require("puppeteer-extra-plugin-stealth/evasions/navigator.vendor");
+require("puppeteer-extra-plugin-stealth/evasions/navigator.webdriver");
+require("puppeteer-extra-plugin-stealth/evasions/sourceurl");
+require("puppeteer-extra-plugin-stealth/evasions/user-agent-override");
+require("puppeteer-extra-plugin-stealth/evasions/webgl.vendor");
+require("puppeteer-extra-plugin-stealth/evasions/window.outerdimensions");
+
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 puppeteer.use(StealthPlugin());
 
